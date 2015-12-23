440729b847230e17c128585ebc0435db7ce77e06
mui-nyan <hngrmi.web@gmail.com>
2015-12-24 00:01:41 +0900

    create command receiver

---
+++ src/jp/muinyan/UsiCommandReceiver.java
@@ -0,0 +1,57 @@
+package jp.muinyan;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * GUIから送られるコマンドを解析する機能を提供するクラスです。
+ *
+ * @author muinyan
+ *
+ */
+public class UsiCommandReceiver {
+
+	private BufferedReader gui;
+
+	private Runnable onUsi;
+
+	public UsiCommandReceiver(InputStream in) {
+
+		this.gui = new BufferedReader(new InputStreamReader(in));
+	}
+
+	/**
+	 * GUIからのコマンド受付を開始します。コマンドを受信すると、対応するコールバックが発火します。
+	 */
+	public void start() {
+		try {
+			while (true) {
+				String command = gui.readLine();
+
+				if (command.equals("usi")) {
+					call(onUsi);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalStateException(e);
+		}
+	}
+
+	/**
+	 * コマンド "usi" に対するコールバックを設定します。
+	 *
+	 * @param callback
+	 */
+	public void setOnUsi(Runnable callback) {
+		this.onUsi = callback;
+	}
+
+	private void call(Runnable runnable) {
+		if (runnable != null) {
+			runnable.run();
+		}
+	}
+}
