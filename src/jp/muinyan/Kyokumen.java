ee66a737a3cc6f6e88873ff5aa95d924e6474f75
mui-nyan <hngrmi.web@gmail.com>
2015-12-26 23:22:56 +0900

    refactoring

--- src/jp/muinyan/Kyokumen.java
+++ src/jp/muinyan/Kyokumen.java
@@ -6,10 +6,62 @@ public class Kyokumen {
 
 	public Kyokumen(String positionString) {
 
-		String s = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL " + "w - 1 moves 5a6b 7g7f 3a3b";
-		String[] lines = s.split(" ")[0].split("/");
+		// positionString "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1 moves 5a6b 7g7f 3a3b";
 
-		assert lines.length == 9;
+		String sfen = positionString.split(" ")[0];
+		bitboard = sfenToBitboard(sfen);
+
+		// TODO movesの解決
+	}
+
+	public void move(String moveString) {
+
+		int fromX = Integer.parseInt(moveString.substring(0, 1)) - 1;
+		int fromY = moveString.charAt(1) - 'a';
+		int toX = Integer.parseInt(moveString.substring(3, 4)) - 1;
+		int toY = moveString.charAt(3) - 'a';
+
+		// 成
+		boolean p = moveString.length() == 5 && moveString.charAt(4) == '+';
+
+		// from,toのbitboard上の座標を求める
+		int fromRow = fromY / 3;
+		int fromCol = fromY % 3 * 9 + fromX;
+
+		int toRow = toY / 3;
+		int toCol = toY % 3 * 9 + toX;
+
+		// 操作対象のbitboardを選択する
+		for (int boardNumber = 0; boardNumber < bitboard.length; boardNumber++) {
+			if ((bitboard[boardNumber][fromRow] & (1 << fromCol)) != 0) {
+
+				// 操作する
+				// fromの座標を0で上書き
+				int mask = 0xFFFFFFFF ^ (1 << fromCol);
+				bitboard[boardNumber][fromRow] &= mask;
+
+				// TODO toの座標に駒があったら取る
+
+				// toの座標を1で上書き
+				// TODO 成はboardNumberを+1する
+				bitboard[boardNumber][toRow] |= (1 << toCol);
+
+				break;
+			}
+		}
+	}
+
+	/**
+	 * sfen形式(moves無し)からbitboardを作成します。
+	 *
+	 * @param sfen
+	 * @return 与えられたsfenが表す局面のbitboard
+	 */
+	public static int[][] sfenToBitboard(String sfen) {
+
+		int[][] bitboard = new int[28][3];
+
+		String[] lines = sfen.split(" ")[0].split("/");
 
 		for (int rowIndex = 0; rowIndex < lines.length; rowIndex++) {
 			String line = lines[rowIndex];
@@ -38,64 +90,7 @@ public class Kyokumen {
 				}
 			}
 		}
-	}
 
-	public void move(String moveString) {
-
-		int fromX = Integer.parseInt(moveString.substring(0, 1)) - 1;
-		int fromY = moveString.charAt(1) - 'a';
-		int toX = Integer.parseInt(moveString.substring(3, 4)) - 1;
-		int toY = moveString.charAt(3) - 'a';
-
-		// 成
-		boolean p = moveString.length() == 5 && moveString.charAt(4) == '+';
-
-		// from,toのbitboard上の座標を求める
-		int fromRow = fromY / 3;
-		int fromCol = fromY % 3 * 9 + fromX;
-
-		int toRow = toY / 3;
-		int toCol = toY % 3 * 9 + toX;
-
-		// 操作対象のbitboardを選択する
-		for(int boardNumber = 0; boardNumber < bitboard.length; boardNumber++) {
-			if((bitboard[boardNumber][fromRow] & (1 << fromCol)) != 0) {
-
-				// 操作する
-				// fromの座標を0で上書き
-				int mask = 0xFFFFFFFF ^ (1 << fromCol);
-				bitboard[boardNumber][fromRow] &= mask;
-
-				// TODO toの座標に駒があったら取る
-
-
-				// toの座標を1で上書き
-				// TODO 成はboardNumberを+1する
-				bitboard[boardNumber][toRow] |= (1 << toCol);
-
-				break;
-			}
-		}
+		return bitboard;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
