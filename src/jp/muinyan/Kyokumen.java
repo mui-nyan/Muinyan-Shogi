1eb88d8cc0ceb121483d839e397950010c28b013
mui-nyan <hngrmi.web@gmail.com>
2016-02-06 21:13:53 +0900

    implement PUT

--- src/jp/muinyan/Kyokumen.java
+++ src/jp/muinyan/Kyokumen.java
@@ -54,19 +54,31 @@ public class Kyokumen {
 	 *
 	 * @param moveString
 	 */
-	public void move(String moveString) {
+	public void analyzeMoveString(String moveString) {
 
-		if (moveString.charAt(2) == '*') {
-			// TODO 打った場合
+		if (moveString.charAt(1) == '*') {
+			// 打った場合
+
+			char koma = moveString.charAt(0);
+			int toX = Integer.parseInt(moveString.substring(3, 4)) - 1;
+			int toY = moveString.charAt(3) - 'a';
+
+			put(koma, toX, toY);
+
+		} else {
+
+			int fromX = Integer.parseInt(moveString.substring(0, 1)) - 1;
+			int fromY = moveString.charAt(1) - 'a';
+			int toX = Integer.parseInt(moveString.substring(3, 4)) - 1;
+			int toY = moveString.charAt(3) - 'a';
+			// 成
+			boolean evolution = moveString.length() == 5 && moveString.charAt(4) == '+';
+
+			move(fromX, fromY, toX, toY, evolution);
 		}
+	}
 
-		int fromX = Integer.parseInt(moveString.substring(0, 1)) - 1;
-		int fromY = moveString.charAt(1) - 'a';
-		int toX = Integer.parseInt(moveString.substring(3, 4)) - 1;
-		int toY = moveString.charAt(3) - 'a';
-
-		// 成
-		boolean evolution = moveString.length() == 5 && moveString.charAt(4) == '+';
+	public void move(int fromX, int fromY, int toX, int toY, boolean evolution) {
 
 		// from,toのbitboard上の座標を求める
 		int fromRow = fromY / 3;
@@ -93,7 +105,7 @@ public class Kyokumen {
 						bitboard[checkTargetBoard][toRow] ^= (1 << toCol);
 
 						// 自分の駒台に加える
-						komadai[getKomadai(checkTargetBoard)] += 1;
+						komadai[boardNumberToKomadaiNumber(checkTargetBoard)] += 1;
 					}
 				}
 
@@ -104,6 +116,19 @@ public class Kyokumen {
 				break;
 			}
 		}
+	}
+
+	private void put(char koma, int x, int y) {
+
+		int boardNumber = charToBoardNumber(koma, false);
+		int toRow = y / 3;
+		int toCol = x % 3 * 9 + x;
+
+		// 指定した位置に駒を置く
+		bitboard[boardNumber][toRow] ^= (1 << toCol);
+
+		// 駒台から駒を減らす
+		komadai[charToKomadaiNumber(koma)] -= 1;
 	}
 
 	/**
@@ -153,17 +178,194 @@ public class Kyokumen {
 	 * 「その駒を取った時に置く駒台」の番号を取得します。
 	 *
 	 * @param boardNumber
-	 * @return
+	 *            bitboardの番号
+	 * @return 駒台の番号 エラーは負数
 	 */
-	private int getKomadai(int boardNumber) {
+	private int boardNumberToKomadaiNumber(int boardNumber) {
 
 		switch (boardNumber) {
 		case GOTE_PORN:
 		case GOTE_PORN_P:
 			return 0;
 
+		case GOTE_LANCE:
+		case GOTE_LANCE_P:
+			return 1;
+
+		case GOTE_KNIGHT:
+		case GOTE_KNIGHT_P:
+			return 2;
+
+		case GOTE_SILVER:
+		case GOTE_SILVER_P:
+			return 3;
+
+		case GOTE_GOLD:
+			return 4;
+
+		case GOTE_ROOK:
+		case GOTE_ROOK_P:
+			return 5;
+
+		case GOTE_BISHOP:
+		case GOTE_BISHOP_P:
+			return 6;
+
+		case SENTE_PORN:
+		case SENTE_PORN_P:
+			return 7;
+
+		case SENTE_LANCE:
+		case SENTE_LANCE_P:
+			return 8;
+
+		case SENTE_KNIGHT:
+		case SENTE_KNIGHT_P:
+			return 9;
+
+		case SENTE_SILVER:
+		case SENTE_SILVER_P:
+			return 10;
+
+		case SENTE_GOLD:
+			return 11;
+
+		case SENTE_ROOK:
+		case SENTE_ROOK_P:
+			return 12;
+
+		case SENTE_BISHOP:
+		case SENTE_BISHOP_P:
+			return 13;
+
 		default:
 			return -1;
 		}
+	}
+
+	/**
+	 * 「その駒が今置かれている駒台」の番号を返します。
+	 *
+	 * @param koma
+	 *            駒の種類
+	 * @return 駒台の番号 エラーは負数
+	 */
+	private int charToKomadaiNumber(char koma) {
+
+		switch (koma) {
+		case 'P':
+			return 0;
+
+		case 'L':
+			return 1;
+
+		case 'N':
+			return 2;
+
+		case 'S':
+			return 3;
+
+		case 'G':
+			return 4;
+
+		case 'R':
+			return 5;
+
+		case 'B':
+			return 6;
+
+		case 'p':
+			return 7;
+
+		case 'l':
+			return 8;
+
+		case 'n':
+			return 9;
+
+		case 's':
+			return 10;
+
+		case 'g':
+			return 11;
+
+		case 'r':
+			return 12;
+
+		case 'b':
+			return 13;
+
+		case 'K':
+			return -1;
+
+		case 'k':
+			return -2;
+		}
+
+		return -9;
+	}
+
+	/**
+	 * 駒の種類からbitboardの番号を取得します。
+	 *
+	 * @param koma
+	 *            駒の種類
+	 * @param evolution
+	 *            成っているかどうか
+	 * @return bitboardの番号 エラーは負数
+	 */
+	private int charToBoardNumber(char koma, boolean evolution) {
+
+		switch (koma) {
+		case 'P':
+			return evolution ? SENTE_PORN_P : SENTE_PORN;
+
+		case 'L':
+			return evolution ? SENTE_LANCE_P : SENTE_LANCE;
+
+		case 'N':
+			return evolution ? SENTE_KNIGHT_P : SENTE_KNIGHT;
+
+		case 'S':
+			return evolution ? SENTE_SILVER_P : SENTE_SILVER;
+
+		case 'G':
+			return evolution ? -1 : SENTE_GOLD;
+
+		case 'R':
+			return evolution ? SENTE_ROOK_P : SENTE_ROOK;
+
+		case 'B':
+			return evolution ? SENTE_BISHOP_P : SENTE_BISHOP;
+
+		case 'K':
+			return evolution ? -2 : SENTE_KING;
+
+		case 'p':
+			return evolution ? GOTE_PORN_P : GOTE_PORN;
+
+		case 'l':
+			return evolution ? GOTE_LANCE_P : GOTE_LANCE;
+
+		case 'n':
+			return evolution ? GOTE_KNIGHT_P : GOTE_KNIGHT;
+
+		case 's':
+			return evolution ? GOTE_SILVER_P : GOTE_SILVER;
+
+		case 'g':
+			return evolution ? -3 : GOTE_GOLD;
+
+		case 'r':
+			return evolution ? GOTE_ROOK_P : GOTE_ROOK;
+
+		case 'b':
+			return evolution ? GOTE_BISHOP_P : GOTE_BISHOP;
+
+		case 'k':
+			return evolution ? -4 : GOTE_KING;
+		}
+
+		return -9;
 	}
 }
